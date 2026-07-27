4d43c4b790a34f86b33fe8f01a3a1d46c354bcec
nxvulent <299918837+nxvulent@users.noreply.github.com>
2026-07-27 02:54:18 +0200

    Delete Wockstarz Multi V2 directory

--- Wockstarz Multi V2/lib/__init__.py
+++
@@ -1,1 +0,0 @@
-# Wock-Multi library package
--- Wockstarz Multi V2/tools/custom/example_plugin.py
+++
@@ -1,7 +0,0 @@
-# Custom plugins for Wock-Tools
-# Drop a .py file here — it appears in PLUGINS category.
-#
-# Example minimal plugin:
-#
-# print("Hello from my plugin!")
-# input("Press Enter...")
